4af1137c72005fb396ba73a24f7e3f10d94727a0
Davide Bragagnolo <davide@crumpler.com.au>
2021-01-27 17:44:14 +1100

    added env variables

---
+++ src/firebase-credentials.js
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DBURL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
+};
